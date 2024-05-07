b97d3cf2457be7b68017ffb591bfb71b4ab01d49
Abi24Francis <abishek.l@digitusbiz.com>
2024-05-07 16:54:13 +0530

    Abi portfolio

---
+++ abi_portfolio/layouts/products.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <nuxt-link to="/products">Get to products page</nuxt-link>
+            </nav>
+        </header>
+
+        <div>
+            <slot />
+        </div>
+        <footer>
+            <ul>
+                    <li> 
+                        <nuxt-link to="/">Home</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/about">About</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/test">Test</nuxt-link>
+                    </li>
+                </ul>
+        </footer>
+    </div>
+</template>
---
+++ abi_portfolio/pages/Test.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Test</h2>
+        <p>Lorem Ipsum dolor</p>
+        <p>Lorem Ipsum dolor</p>
+    
+    </div>
+    </template>
+    
+    <script>
+    definePageMeta({
+	layout: 'products'
+})
+    </script>
+    
+    <style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+        
+    }
+    </style>
